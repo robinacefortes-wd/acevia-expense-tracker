701c67e41472c5db747520fcc000ff7cdba6df60
Robin Ace Fortes <robinacefortes@gmail.com>
2026-02-25 12:49:35 +0800

    fix: align registration test with first_name/last_name/phone schema

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,6 +1,8 @@
 <?php
 
 test('registration screen can be rendered', function () {
+    $this->withoutVite();
+
     $response = $this->get(route('register'));
 
     $response->assertOk();
@@ -8,9 +10,11 @@ test('registration screen can be rendered', function () {
 
 test('new users can register', function () {
     $response = $this->post(route('register.store'), [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
+        'first_name'            => 'Test',
+        'last_name'             => 'User',
+        'phone'                 => '1234567890',
+        'email'                 => 'test@example.com',
+        'password'              => 'password',
         'password_confirmation' => 'password',
     ]);
 
