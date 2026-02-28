3aa0b4a68b7f9294ce7666c36d4f3132a24fd6e3
Robin Ace Fortes <robinacefortes@gmail.com>
2026-03-01 01:32:55 +0800

    fix: lowercase pages directory in Vite glob

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -14,9 +14,9 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.tsx`,
-            import.meta.glob('./Pages/**/*.tsx'),
+    resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.tsx'),
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
